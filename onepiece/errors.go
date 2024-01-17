7ac35afe5fd7ec1595c795501b327902384becd9
Yordis Prieto <yordis.prieto@gmail.com>
2024-01-17 15:56:20 -0500

    feat: add type provider

--- onepiece/errors.go
+++ onepiece/errors.go
@@ -5,3 +5,4 @@ import "errors"
 var ErrTerminalState = errors.New("state is terminal")
 var ErrUnknownCommand = errors.New("unknown command")
 var ErrUnknownEvent = errors.New("unknown event")
+var ErrUnknownMessage = errors.New("unknown message")
