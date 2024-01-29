2ecf9c836317ed7c270f01ea7f137e3d09798ad8
Yordis Prieto <yordis.prieto@gmail.com>
2024-01-28 20:44:52 -0500

    PogPaused

--- unstable/rustlang/crates/monitoring-wasm/src/lib.rs
+++ unstable/rustlang/crates/monitoring-wasm/src/lib.rs
@@ -1,10 +1,5 @@
 use extism_pdk::*;
 
-#[derive(serde::Deserialize)]
-struct EventState {
-    pub event: monitoring::Event,
-    pub state: monitoring::State,
-}
 
 #[plugin_fn]
 pub fn stream_id(Json(command): Json<monitoring::Command>) -> FnResult<String> {
@@ -22,10 +17,51 @@ pub fn initial_state(_input: ()) -> FnResult<Json<monitoring::State>> {
     Ok(Json(monitoring::initial_state()))
 }
 
+#[derive(serde::Deserialize)]
+struct EvolveCommand {
+    pub event: monitoring::Event,
+    pub state: monitoring::State,
+}
+
 #[plugin_fn]
-pub fn evolve(Json(event_state): Json<EventState>) -> FnResult<Json<monitoring::State>> {
+pub fn evolve(Json(event_state): Json<EvolveCommand>) -> FnResult<Json<monitoring::State>> {
     Ok(Json(monitoring::evolve(
         &event_state.state,
         &event_state.event,
     )))
+}
+
+#[plugin_fn]
+pub fn is_terminal(Json(state): Json<monitoring::State>) -> FnResult<Json<bool>> {
+    Ok(Json(monitoring::is_terminal(&state)))
+}
+
+#[plugin_fn]
+pub fn event_type(Json(event): Json<monitoring::Event>) -> FnResult<String> {
+    let event_type = match event {
+        monitoring::Event::MonitoringStarted { .. } => "MonitoringStarted",
+        monitoring::Event::MonitoringPaused { .. } => "MonitoringPaused",
+        monitoring::Event::MonitoringResumed { .. } => "MonitoringResumed",
+    };
+    Ok(event_type.to_string())
+}
+
+#[plugin_fn]
+pub fn marshal_event(Json(event): Json<monitoring::Event>) -> FnResult<Vec<u8>> {
+    Ok(serde_json::to_vec(&event)?)
+}
+
+#[derive(Debug, serde::Deserialize)]
+struct UnmarshalEventCommand {
+    pub event_type: String,
+    pub payload: String
+}
+
+#[plugin_fn]
+pub fn unmarshal_event(Json(command): Json<UnmarshalEventCommand>) -> FnResult<Json<monitoring::Event>> {
+    debug!("unmarshal_event: {:?}", command);
+    let u: monitoring::Event = serde_json::from_str(&command.payload.as_str())?;
+    println!("{:#?}", u);
+
+    Ok(Json(u))
 }
--- unstable/rustlang/crates/runtime/src/main.rs
+++ unstable/rustlang/crates/runtime/src/main.rs
@@ -7,8 +7,61 @@ fn main() {
     let manifest = extism::Manifest::new([url]);
     let mut plugin = extism::Plugin::new(&manifest, [], true).unwrap();
 
-    // let input = r#"{"id": "1", "url": "https://www.youtube.com/watch?v=dQw4w9WgXcQ"}"#;
-    // let stream_id = plugin.call::<&str, &str>("stream_id", input).unwrap();
+    let input = r#"
+        {
+          "CreateMonitoring": {"id": "1", "url": "https://www.youtube.com/watch?v=dQw4w9WgXcQ"}
+        }
+      "#;
+    let stream_id: String = plugin.call("stream_id", input).unwrap();
+    println!("{}", stream_id);
     let state: String = plugin.call("initial_state", "").unwrap();
     println!("{}", state);
+
+    let input = r#"
+        {
+          "state": {"id":null,"status":"Paused"},
+          "event": {
+            "MonitoringStarted": {"id": "1", "url": "https://www.youtube.com/watch?v=dQw4w9WgXcQ"}
+          }
+        }
+      "#;
+    let state: String = plugin.call("evolve", input).unwrap();
+    println!("{}", state);
+
+    let input = r#"
+        {"id":null,"status":"Paused"}
+      "#;
+    let is_terminal: String = plugin.call("is_terminal", input).unwrap();
+    println!("{}", is_terminal);
+
+    let input = r#"
+        {
+            "MonitoringStarted": {"id": "1", "url": "https://www.youtube.com/watch?v=dQw4w9WgXcQ"}
+          }
+      "#;
+    let event_type: String = plugin.call("event_type", input).unwrap();
+    println!("{}", event_type);
+
+    let marshal_event: String = plugin
+        .call(
+            "marshal_event",
+            r#"
+        {
+            "MonitoringStarted": {"id": "1", "url": "https://www.youtube.com/watch?v=dQw4w9WgXcQ"}
+          }
+      "#,
+        )
+        .unwrap();
+    println!("{}", marshal_event);
+
+    let payload = serde_json::json!({
+        "event_type": "MonitoringStarted",
+        "payload": serde_json::json!({
+          "MonitoringStarted": {"id": "1", "url": "https://www.youtube.com/watch?v=dQw4w9WgXcQ"}
+        }).to_string()
+    })
+    .to_string();
+
+    let unmarshal_event: String = plugin.call("unmarshal_event", payload).unwrap();
+    println!("{}", unmarshal_event);
 }
